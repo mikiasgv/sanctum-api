0afe19c72cc8dd239abdbb7ec4c5f568f33a8180
mikiasgv <mikiasgv@gmail.com>
2021-03-30 13:09:02 -0400

    bug fix

--- app/Http/Requests/PlaceRequest.php
+++ app/Http/Requests/PlaceRequest.php
@@ -29,7 +29,7 @@ class PlaceRequest extends FormRequest
             'category' => ['required'],
             'name' => ['required'],
             'location' => ['required'],
-            'number' => ['required']
+            'rating' => ['required']
         ];
     }
 }
