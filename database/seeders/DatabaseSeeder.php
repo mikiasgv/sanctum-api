2f7cc3eb503ee6231534b7b93b88b855eb2974d4
mikiasgv <mikiasgv@gmail.com>
2021-03-30 11:11:45 -0400

    fixing unique values

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -26,7 +26,7 @@ class DatabaseSeeder extends Seeder
 
                 \App\Models\Alert::factory()->create([
                     'event_id' => $event->id,
-                    'original_alert_id' => $faker->create()->sentence(1),
+                    'original_alert_id' => $faker->create()->uniqid,
                     'category' => ['Travel', 'Disaster', 'Health', 'Weather'][rand(0, 3)],
                     'name' => $faker->create()->sentence(3),
                     'body' => $faker->create()->sentence(15),
@@ -39,7 +39,7 @@ class DatabaseSeeder extends Seeder
 
                 \App\Models\Place::factory()->create([
                     'event_id' => $event->id,
-                    'original_place_id' => $faker->create()->sentence(1),
+                    'original_place_id' => $faker->create()->uniqid,
                     'category' => ['Hotels', 'Hospitals', 'Restaurants'][rand(0, 2)],
                     'name' => $faker->create()->sentence(3),
                     'number' => $faker->create()->randomNumber(),
